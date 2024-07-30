1a8751c8e88cae26caba62a7558acf1648499197
SebastianAlturckCarlos <142696037+SebastianAlturckCarlos@users.noreply.github.com>
2024-07-30 18:10:54 -0500

    dd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,23 +12,12 @@ import { AuthProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: "dark" }} // Set the default color scheme here
-    >
-      <Router>
-        <App />
-      </Router>
-=======
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <AuthProvider>
         <Router>
           <App />
         </Router>
       </AuthProvider>
->>>>>>> b7055d75778a35b63c2e6378c0e4d156bb8e3bcb
     </MantineProvider>
   </React.StrictMode>
 );
