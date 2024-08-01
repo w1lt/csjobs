7a133fd63a6fce7147a4c616ce4a8e22c36a8ae8
William Whitehead <willclashtest@gmail.com>
2024-08-01 16:24:04 -0500

    refactor: Update Footer component to remove unused code and improve server status link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,5 @@
 import React from "react";
-import { Container, Text, Space, Center, Anchor, Group } from "@mantine/core";
-import {
-  IconBrandLinkedin,
-  IconBrandGithub,
-  IconWorldWww,
-  IconListLetters,
-} from "@tabler/icons-react";
-import { Flex } from "@mantine/core";
+import { Container, Text, Space, Anchor } from "@mantine/core";
 
 const Footer = () => {
   return (
@@ -24,46 +17,6 @@ const Footer = () => {
         </Anchor>
       </Text>
       <Space h="md" />
-      <Center>
-        <Group mb="sm" position="center" spacing="md">
-          <Flex
-            mih={50}
-            gap="md"
-            justify="flex-start"
-            align="flex-start"
-            direction="row"
-            wrap="wrap"
-          >
-            <Anchor href="https://linked.whitehead.wiki/" target="_blank">
-              <IconBrandLinkedin size={24} className="icon-hover" />
-            </Anchor>
-            <Anchor href="https://git.whitehead.wiki/" target="_blank">
-              <IconBrandGithub size={24} className="icon-hover" />
-            </Anchor>
-            <Anchor href="https://whitehead.wiki/" target="_blank">
-              <IconWorldWww size={24} className="icon-hover" />
-            </Anchor>
-            <Text mb={12} align="center">
-              |
-            </Text>
-            <Anchor
-              href="https://www.linkedin.com/in/sebastian-alturck-carlos/"
-              target="_blank"
-            >
-              <IconBrandLinkedin size={24} className="icon-hover" />
-            </Anchor>
-            <Anchor
-              href="https://github.com/SebastianAlturckCarlos"
-              target="_blank"
-            >
-              <IconBrandGithub size={24} className="icon-hover" />
-            </Anchor>
-            <Anchor href="/todo">
-              <IconListLetters size={24} className="icon-hover" />
-            </Anchor>
-          </Flex>
-        </Group>
-      </Center>
     </Container>
   );
 };
