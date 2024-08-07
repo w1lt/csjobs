1c684bd8fef81d17de8bab321e052e0d4fa4ae40
SebastianAlturckCarlos <142696037+SebastianAlturckCarlos@users.noreply.github.com>
2024-08-07 13:15:46 -0500

    help center

--- frontend/src/components/HelpModal.js
+++ frontend/src/components/HelpModal.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Anchor, Text } from "@mantine/core";
+import { Anchor, Text, Stack, Divider, Group } from "@mantine/core";
 
 const HelpModal = () => {
   return (
-    <>
-      <Text mb="sm">
+    <Stack spacing="sm">
+      <Text>
         Feel free to reach out to us at{" "}
-        <Anchor href="mailto:hello@csjobs.lol">hello@csjobs.lol</Anchor>{" "}
+        <Anchor href="mailto:hello@csjobs.lol">hello@csjobs.lol</Anchor>
       </Text>
-    </>
+
+      <Divider />
+
+      <Text>Credits</Text>
+      <Group spacing="xs" direction="column">
+        <Anchor href="https://www.linkedin.com/in/willwhitehead122/">
+          Will Whitehead
+        </Anchor>
+        <Anchor href="https://www.linkedin.com/in/sebastian-alturck-carlos/">
+          Sebastian Alturck-Carlos
+        </Anchor>
+      </Group>
+    </Stack>
   );
 };
 
